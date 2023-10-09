0eb97f91104a1ae92d9b8c30c87aefcabc728714
Anshul <anshulsoni2003@gmail.com>
2023-10-09 23:52:20 +0530

    feat: Responsiveness home-hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,25 +7,34 @@ import LinkedIn from '../static/LinkedIn.png';
 import Hashnode from '../static/Hashnode.png';
 import TwitterX from '../static/TwitterX.png';
 import GitHub from '../static/GitHub.png';
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
+    <div className='flex flex-col'>
+        <div className='bg-lineColor border-solid w-full h-[1.5px]'><hr></hr></div>
     <div className='flex flex-row'>
-      <div className='w-[35%] ml-10 py-20 mt-8'>
-        <div className='mb-6'>
+      <div className='w-[32%] max-xxs:w-full max-xs:w-full max-md:w-[32%] ml-24 max-xxs:ml-2 max-xs:ml-4 max-md:ml-10 max-lg:ml-12 max-xl:ml-16 py-20 max-xxs:py-0 max-xs:py-2 max-md:py-10 max-lg:py-14 mt-8 max-xxs:mt-2 max-xs:mt-4 max-sm:mt-6 max-md:mt-6 max-lg:mt-6'>
+        <div className='mb-6 max-xxs:mb-1 max-xs:mb-2'>
           <img
             src={gdscLight}
             alt="GDSC Light"
-            className='w-[20%]'
+            className='w-[20%] max-xxs:w-[10%] max-xs:w-[15%] max-md:w-[20%] max-lg:w-[25%] max-xl:w-[25%]'
           />
         </div>
 
-        <div className='leading-normal text-greyText drop-shadow-custom '>
-          <p className='font-bold text-6xl leading-normal'>Google Developer<br />
-            <span className='leading-normal'> Student Club</span></p>
-          <p className='font-semibold text-4xl mt-4'>UPES Dehradun</p>
+        <div className='text-greyText drop-shadow-custom'>
+          <div className='flex flex-col max-xs:flex-row font-bold text-6xl max-xxs:text-xl max-xs:text-2xl max-sm:text-2xl max-md:text-3xl max-lg:text-4xl max-xl:text-5xl'>
+            <div className='flex leading-normal'>
+            <p className='whitespace-pre'>Google Developer </p>
+            </div>
+            <div className='flex leading-normal'>
+            <p>Student Club</p>
+            </div>
+          </div>
+          <p className='font-semibold text-5xl max-xxs:text-lg max-xs:text-lg max-sm:text-xl max-md:text-2xl max-lg:text-3xl max-xl:text-4xl mt-4 max-xxs:mt-1 max-xs:mt-2 max-sm:mt-3'>UPES Dehradun</p>
         </div>
 
-        <div className='flex py-10 text-3xl'>
+        <div className='flex py-10 max-xs:py-5 max-sm:py-5 max-md:py-7 max-lg:py-9 text-3xl max-xxs:text-lg max-xs:text-lg max-sm:text-lg max-md:text-2xl max-lg:text-2xl max-xl:text-3xl'>
           <p>
             <span className='text-redText font-bold drop-shadow-custom'>Code.</span>
             <span className='text-blueText font-bold drop-shadow-custom px-2'>Create.</span>
@@ -33,23 +42,36 @@ const Home = () => {
           </p>
         </div>
 
-        <div className='socialmedia flex flex-row'>
-          <img src={Website} alt="Website" className='w-12 mr-10' />
-          <img src={Instagram} alt="Instagram" className='w-12 mr-10' />
-          <img src={LinkedIn} alt="LinkedIn" className='w-12 mr-10' />
-          <img src={Hashnode} alt="Hashnode" className='w-12 mr-10' />
-          <img src={TwitterX} alt="Twitter" className='w-12 mr-10' />
-          <img src={GitHub} alt="GitHub" className='w-12 mr-10' />
-        </div>
+        <div className='flex flex-row'>
+          <Link  to="#">
+            <img src={Website} alt="Website" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 max-lg:w-8 max-xl:w-10  mr-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+          <Link to="https://instagram.com/">
+            <img src={Instagram} alt="Instagram" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 max-lg:w-8 max-xl:w-10 mr-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+          <Link to="https://linkedin.com/">
+            <img src={LinkedIn} alt="LinkedIn" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 mr-10 max-lg:w-8 max-xl:w-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+          <Link to="https://hashnode.com/">
+            <img src={Hashnode} alt="Hashnode" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 mr-10 max-lg:w-8 max-xl:w-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+          <Link to="https://twitter.com">
+            <img src={TwitterX} alt="TwitterX" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 mr-10 max-lg:w-8 max-xl:w-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+          <Link to="https://github.com/">
+            <img src={GitHub} alt="GitHub" className='w-12 max-xxs:w-7 max-xs:w-7 max-sm:w-6 max-md:w-8 max-lg:w-8 max-xl:w-10 mr-10 max-xxs:mr-5 max-xs:mr-7 max-sm:mr-6 max-md:mr-8' />
+          </Link>
+        </div>  
       </div>
 
-      <div className=' w-[65%] flex'>
+      <div className='w-[68%] max-xxs:w-0 max-xs:w-0 max-md:w-[68%] flex'>
         <img
           src={homeHero}
           alt="Home Hero"
-          className='w-full'
+          className='w-full h-full'
         />
       </div>
+    </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,13 +9,21 @@ module.exports = {
         'greyText': '#626262',
         'redText':'#EA4335',
         'blueText': '#0060FF',
-        'greenText': '#0D9C58'
+        'greenText': '#0D9C58',
+        'lineColor':'#C5C5C5'
       },
       dropShadow:{
         'custom': '0px 4px 4px rgba(0, 0, 0, 0.25)',
       },
+      screens:{
+        'xxs': '380px',
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px'
+      },
     },
-
   },
   plugins: [],
 }
